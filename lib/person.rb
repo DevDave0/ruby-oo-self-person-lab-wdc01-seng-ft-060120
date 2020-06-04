5205948b3f9e7bc9bf9221f9ac8a1018d3500d17
devdave0 <dbchung24@gmail.com>
2020-06-04 14:53:45 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -10,57 +10,63 @@ class Person
         @hygiene = hygiene_points
     end 
 
+    # def happiness=(happiness_points)
+    #     @happiness = happiness_points.clamp(0, 10)
+    # end 
+
     def happiness=(happiness_points)
-        @happiness = happiness_points.clamp(0, 10)
-    end 
-
-    def hygiene=(hygiene_points)
-        @hygiene = hygiene_points.clamp(0,10)
-    end 
-
-    def happy?
-        self.happiness > 7 ? true : false
-    end 
-
-    def clean?
-        self.hygiene > 7 ? true : false 
-    end 
-
-    def get_paid(salary)
-        @bank_account += salary
-        'all about the benjamins'
-    end 
-
-    def take_bath 
-        self.hygiene += 4
-        '♪ Rub-a-dub just relaxing in the tub ♫'
-    end 
-
-    def work_out
-        self.hygiene -= 3
-        self.happiness += 2
-        '♪ another one bites the dust ♫'
-    end 
-
-    def call_friend(person)
-        person.happiness += 3
-        self.happiness += 3
-        "Hi #{person.name}! It's #{self.name}. How are you?"
-    end 
-
-    def start_conversation(person, topic)
-        if topic == 'politics'
-            self.happiness -= 2
-            person.happiness -= 2
-            'blah blah partisan blah lobbyist'
-        elsif topic == 'weather'
-            self.happiness += 1
-            person.happiness += 1
-            'blah blah sun blah rain'
-        else 
-            'blah blah blah blah blah'
+        if happiness_points = (0..10).minmax
+        @happiness = happiness_points
         end 
-
     end 
+
+    # def hygiene=(hygiene_points)
+    #     @hygiene = hygiene_points.clamp(0,10)
+    # end 
+
+    # def happy?
+    #     self.happiness > 7 ? true : false
+    # end 
+
+    # def clean?
+    #     self.hygiene > 7 ? true : false 
+    # end 
+
+    # def get_paid(salary)
+    #     @bank_account += salary
+    #     'all about the benjamins'
+    # end 
+
+    # def take_bath 
+    #     self.hygiene += 4
+    #     '♪ Rub-a-dub just relaxing in the tub ♫'
+    # end 
+
+    # def work_out
+    #     self.hygiene -= 3
+    #     self.happiness += 2
+    #     '♪ another one bites the dust ♫'
+    # end 
+
+    # def call_friend(person)
+    #     person.happiness += 3
+    #     self.happiness += 3
+    #     "Hi #{person.name}! It's #{self.name}. How are you?"
+    # end 
+
+    # def start_conversation(person, topic)
+    #     if topic == 'politics'
+    #         self.happiness -= 2
+    #         person.happiness -= 2
+    #         'blah blah partisan blah lobbyist'
+    #     elsif topic == 'weather'
+    #         self.happiness += 1
+    #         person.happiness += 1
+    #         'blah blah sun blah rain'
+    #     else 
+    #         'blah blah blah blah blah'
+    #     end 
+
+    # end 
 
 end 
